638e7013454d03e569b1d48f4771ff80c8864e2d
Windows Phone <venkat@venkatas-macbook-pro.local>
2017-02-08 10:52:53 -0600

    x

---
+++ iosFeatures/MapViewController.h
@@ -0,0 +1,26 @@
+//
+//  MapViewController.h
+//  iosFeatures
+//
+//  Created by Venkata  naraharisetty on 1/24/17.
+//  Copyright © 2017 Venkata  naraharisetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "SearchLocationViewController.h"
+
+
+@interface MapViewController : UIViewController <MKMapViewDelegate,CLLocationManagerDelegate,sendDestinationData>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (retain,nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) CLLocation * startLocation;
+@property (nonatomic) MKCoordinateRegion startRegion;
+@property (nonatomic) CLLocation * destinationLocation;
+@property (nonatomic) CLPlacemark *destinationPlaceMark;
+@property (nonatomic,retain) CLGeocoder  *coder;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *selectDestinationButton;
+
+@end
