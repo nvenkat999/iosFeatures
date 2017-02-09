af287935950e6117047bd49f1613c9870e1c3172
Venkata Naraharisetty <nvenkat999@yahoo.com>
2017-02-09 10:23:43 -0600

    n

---
+++ iosFeatures/iosFeatures/iosFeatures/iosFeatures/iosFeatures/iosFeatures/iosFeatures/iosFeatures/iosFeatures/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  iosLearningProject
+//
+//  Created by Venkata  naraharisetty on 12/21/16.
+//  Copyright © 2016 Venkata  naraharisetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITableViewController
+
+@property (weak,nonatomic) IBOutlet UIBarButtonItem *barButton;
+
+@end
