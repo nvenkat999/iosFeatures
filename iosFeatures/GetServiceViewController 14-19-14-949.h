1bfe3db520e2011314189ab8dcaac107612cdd3d
Venkata Naraharisetty <nvenkat999@yahoo.com>
2017-02-09 10:15:41 -0600

    s

---
+++ iosFeatures/GetServiceViewController 14-19-14-949.h
@@ -0,0 +1,16 @@
+//
+//  GetServiceViewController.h
+//  iosLearningProject
+//
+//  Created by Venkata  naraharisetty on 12/28/16.
+//  Copyright © 2016 Venkata  naraharisetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetServiceViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet  UIBarButtonItem *barButton;
+
+
+@end
